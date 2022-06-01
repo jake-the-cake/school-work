e16235f8c8a0e4efde0476601795da5d5934c863
jake-the-cake <askjake331@gmail.com>
2022-06-01 15:01:01 -0400

    random negatives and filter out 0 or 1

--- week8/hello/script.js
+++ week8/hello/script.js
@@ -1,13 +1,48 @@
-// create random numbers
-const randomA = parseInt(Math.random() * 100);
-const randomB = parseInt(Math.random() * 10);
-const result = randomA * randomB;
+// create random number
+const randomInteger = () => {
+    return parseInt(Math.random() * 10)
+}
 
+const getNumbers = () => {
+    let numbers = randomPositiveNegative([randomInteger(),randomInteger()]);
+    console.log(numbers[0], numbers[1]);
+    if (checkForZeroOrOne(numbers)) {
+        return getNumbers();
+    }
+    else {
+        return numbers;  
+    }
+}
+
+// check for 0 or 1
+const checkForZeroOrOne = (numbers) => {
+    let containsZeroOrOne = false;
+    numbers.forEach((number) => {
+        if (number === 1 || number === 0) {
+            containsZeroOrOne = true;
+        }
+    });
+    return containsZeroOrOne;
+}
+
+// randomly assign negatives
+const randomPositiveNegative = (numbers) => {
+    numbers.forEach((number, index) => {
+        const random = randomInteger();
+        if (random % 2 === 0) {
+            numbers[index] *= -1;
+        }
+    });
+    return numbers;
+}
+
+let randomNumbers = getNumbers();
+const result = randomNumbers[0] * randomNumbers[1];
 
 const pageLoaded = () => {
     document.getElementById('loader').style.display = 'none';
     document.querySelector('h1').innerHTML = `
-        <p>if <span>x = ${randomA}</span> and <span>y = ${randomB}</span></p>
+        <p>if <span>x = ${randomNumbers[0]}</span> and <span>y = ${randomNumbers[1]}</span></p>
         <p>result <span>x * y =</span> ${result}</p>
     `;
     console.log(`result: ${result}`);
@@ -15,4 +50,5 @@ const pageLoaded = () => {
 
 window.addEventListener('load', () => {
     setTimeout(pageLoaded, 500);
+    //pageLoaded();
 });
