5fbe750ebf9474fe1a6ec2465a776e80f3452303
jake-the-cake <askjake331@gmail.com>
2022-06-01 15:48:25 -0400

    use injectable html elements

--- week8/hello/script.js
+++ week8/hello/script.js
@@ -1,16 +1,17 @@
 // create random number
-const randomInteger = () => {
-    return parseInt(Math.random() * 10)
+const randomInteger = (multiplier) => {
+    return parseInt(Math.random() * multiplier)
 }
 
+// return 2 numbers
 const getNumbers = () => {
-    let numbers = randomPositiveNegative([randomInteger(),randomInteger()]);
-    console.log(numbers[0], numbers[1]);
+    const [ num1, num2 ] = randomPositiveNegative([randomInteger(100),randomInteger(10)]);
+    const numbers = [num1,num2];
     if (checkForZeroOrOne(numbers)) {
         return getNumbers();
     }
     else {
-        return numbers;  
+        return numbers;
     }
 }
 
@@ -28,27 +29,48 @@ const checkForZeroOrOne = (numbers) => {
 // randomly assign negatives
 const randomPositiveNegative = (numbers) => {
     numbers.forEach((number, index) => {
-        const random = randomInteger();
+        const random = randomInteger(10);
         if (random % 2 === 0) {
-            numbers[index] *= -1;
+            numbers[index] = number * -1;
         }
     });
     return numbers;
 }
 
-let randomNumbers = getNumbers();
-const result = randomNumbers[0] * randomNumbers[1];
-
-const pageLoaded = () => {
+// remove loader screen
+const killLoader = () => {
     document.getElementById('loader').style.display = 'none';
-    document.querySelector('h1').innerHTML = `
-        <p>if <span>x = ${randomNumbers[0]}</span> and <span>y = ${randomNumbers[1]}</span></p>
-        <p>result <span>x * y =</span> ${result}</p>
-    `;
-    console.log(`result: ${result}`);
 }
 
+// assemble injectable content
+const displayResults = () => {
+    const [ num1, num2 ] = getNumbers();
+    const result = num1 * num2;
+
+    const inputLine = document.createElement('p');
+    const outputLine = document.createElement('p');
+    const numA = document.createElement('span');
+    const numB = document.createElement('span');
+    const resultRtn = document.createElement('span');
+
+    numA.innerText = `x = ${num1}`;
+    numB.innerText = `y = ${num2}`;
+    resultRtn.innerText = result;
+
+    inputLine.innerHTML = `if ${numA.outerHTML} and ${numB.outerHTML}`;
+    outputLine.innerHTML = `then x * y = ${result}`;
+
+    return inputLine.outerHTML + outputLine.outerHTML;
+}
+
+// main page load function
+const pageLoaded = () => {
+    document.getElementById('root').innerHTML = displayResults();
+    killLoader();
+}
+
+// listen for page load
 window.addEventListener('load', () => {
-    setTimeout(pageLoaded, 500);
-    //pageLoaded();
+    //setTimeout(pageLoaded, 500);
+    pageLoaded();
 });
