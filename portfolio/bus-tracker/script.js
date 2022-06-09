abcec1cd5193c9dcbd592d7920d2262f561758b4
jake-the-cake <askjake331@gmail.com>
2022-06-09 14:59:34 -0400

    make map work with bus api

--- portfolio/bus-tracker/script.js
+++ portfolio/bus-tracker/script.js
@@ -1,4 +1,5 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicGFwYWpha2VkZXYiLCJhIjoiY2w0NzBnODE1MGRjYzNjb3J0Z3pwZTJmOCJ9.lAFCXMtgFqBMGhF5QVXAgg'
+const currentNarkers = []
 
 const map = new mapboxgl.Map({
   container: 'map',
@@ -7,6 +8,29 @@ const map = new mapboxgl.Map({
   zoom: 14
 })
 
-const marker = new mapboxgl.Marker()
-	.setLngLat([-71.092761, 42.357575])
-	.addTo(map)
+async function run(){
+	const locations = await getBusLocations();
+	console.log(` refreshed at ${new Date()}`);
+	await currentNarkers.forEach((marker, index) => {
+		currentNarkers[index].remove()
+		currentNarkers.shift()
+	})
+
+	locations.forEach(location => {
+		const lng = location.attributes.longitude
+		const lat = location.attributes.latitude
+		const marker = new mapboxgl.Marker()
+			.setLngLat([lng,lat])
+			.addTo(map)
+		currentNarkers.push(marker)
+	});
+	
+	setTimeout(run, 15000);
+}
+
+async function getBusLocations(){
+	const url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';
+	const response = await fetch(url);
+	const json = await response.json();
+	return json.data;
+}
