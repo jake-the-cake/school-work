71777b585fcc80645cbefaa08ebb09a7e4fbbee7
jake-the-cake <askjake331@gmail.com>
2022-08-04 16:02:24 -0400

    axios works!

--- week15/fetch-rebuild/script.jsx
+++ week15/fetch-rebuild/script.jsx
@@ -1,6 +1,24 @@
-const { Fragment } = React
+const { Fragment, useState, useEffect } = React
+
+const getNumbers = async () => {
+    const url = 'http://numbersapi.com/2'
+    const data = await axios(url).then((data) => data)
+    console.log(data)
+    return data
+}
 
 const NumberGame = () => {
+    
+    const [number, setNumber] = useState(null)
+    
+    useEffect(() => {
+        const testAxios = (async () => {
+            const data = await getNumbers().then(({data}) => data)
+            const formattedData = await data.split(' ').slice(2).join(' ')
+            setNumber(formattedData[0].toUpperCase() + formattedData.slice(1))
+        })()
+    }, [])
+
     return (
         <Fragment>
             <div className="number-cell">42</div>
@@ -12,6 +30,7 @@ const NumberGame = () => {
                 <div className="answer-cell">D</div>
             </div>
             <div className="score-block">0 out of 0 : 0%</div>
+            {number}
         </Fragment>
     )
 }
