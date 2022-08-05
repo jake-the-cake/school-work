062bf050979634031ff6fdc04a97b100a4ffdcbb
jake-the-cake <askjake331@gmail.com>
2022-08-04 20:38:50 -0400

    finish first demo version on fetch rebuild

---
+++ week15/fetch-rebuild/README.md
@@ -0,0 +1,36 @@
+## Number API Trivia Game
+
+Description:
+- Using an API provided by numbersapi.com, this app generates a new, randomized game of trivia, with 3 unique decoy answers.
+
+How to run:
+- Visit https://jake-the-cake.github.io/school-work/week15/fetch-rebuild/index.html
+- No installation required
+- The page will load the question automatically
+- Click on your best guess
+- Selection results display in the console.
+
+Roadmap:
+- Score recorder
+- Results that show in browser
+- Button to load a new question after answer
+
+Copyright (c) 2022 Jake Thompson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- week15/fetch-rebuild/script.jsx
+++ week15/fetch-rebuild/script.jsx
@@ -1,36 +1,82 @@
 const { Fragment, useState, useEffect } = React
 
-const getNumbers = async () => {
-    const url = 'http://numbersapi.com/2'
+const getNumbers = async (number) => {
+    const url = `http://numbersapi.com/${number}/trivia`
     const data = await axios(url).then((data) => data)
-    console.log(data)
-    return data
+    return await data
+}
+
+const createNewQuestion = async (setNumber, answers, setAnswers, setCurrentQuestion) => {
+    const answerArray = []
+    let answerHolder
+    for (let i=1; i <= 4; i++) {
+        let isValidating = true
+        while (isValidating === true) {
+            const data = await getNumbers(parseInt(Math.random() * 101)).then(({data}) => data)
+            const splitData = await data.split(' ')
+            const textFromData = splitData.slice(2).join(' ')
+            const formattedTextFromData = textFromData[0].toUpperCase() + textFromData.slice(1)
+            if (i === 1) {
+                setNumber(splitData[0])
+                    answerHolder = splitData[0]
+                    answerArray.push(formattedTextFromData)
+                    isValidating = false
+            }
+            else {
+                let isUnique = true
+                answerArray.forEach(answer => {
+                    if (answer === formattedTextFromData) {
+                        isUnique = false
+                    }
+                })
+                if (isUnique === true) {
+                    answerArray.push(formattedTextFromData)
+                    isValidating = false
+                }
+            }            
+        }
+    }
+    setAnswers(answerArray)
+    const createAnswerGrid = ([answer, ...rest]) => {
+        const answerGrid = [...rest]
+        const answerPosition = parseInt(Math.random() * 4)
+        answerGrid.splice(answerPosition, 0, answer)
+        return answerGrid
+    }
+    const gridData = {number: answerHolder, text: answerArray[0], grid: createAnswerGrid(answerArray)}
+    setCurrentQuestion(gridData)
 }
 
 const NumberGame = () => {
-    
     const [number, setNumber] = useState(null)
+    const [answers, setAnswers] = useState([])
+    const [currentQuestion, setCurrentQuestion] = useState([])
     
     useEffect(() => {
-        const testAxios = (async () => {
-            const data = await getNumbers().then(({data}) => data)
-            const formattedData = await data.split(' ').slice(2).join(' ')
-            setNumber(formattedData[0].toUpperCase() + formattedData.slice(1))
-        })()
+        createNewQuestion(setNumber, answers, setAnswers, setCurrentQuestion)
     }, [])
+
+    const handleSubmitAnswer = (e) => {
+        const selectedAnswer = e.target.innerText
+        console.log('%cSelection:', 'color:red', selectedAnswer)
+        if (selectedAnswer === currentQuestion.text) {
+            console.log('%cResult:', 'color:blue', 'CORRECT')
+        }
+        else {
+            console.log('%cResult:', 'color:blue', 'Wrong Answer')
+        }
+    }
 
     return (
         <Fragment>
-            <div className="number-cell">42</div>
+            <div className="instruction">Your Number Is:</div>
+            <div className="number-cell">{number}</div>
             <div className="instruction">Which of these facts is about that number?</div>
             <div className="answer-block">
-                <div className="answer-cell">A</div>
-                <div className="answer-cell">B</div>
-                <div className="answer-cell">C</div>
-                <div className="answer-cell">D</div>
+                { currentQuestion?.grid?.map((answer, index) => {
+                    return <div className='answer-button' onClick={handleSubmitAnswer} id ={`answer${index}`} key={`answer${index}`}>{answer}</div>
+                }) }
             </div>
-            <div className="score-block">0 out of 0 : 0%</div>
-            {number}
         </Fragment>
     )
 }
