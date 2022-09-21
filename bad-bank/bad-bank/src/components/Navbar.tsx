27d08467e961f2bc8ab782951e188521f650078d
jake-the-cake <askjake331@gmail.com>
2022-09-21 11:54:04 -0400

    navbar messages

--- bad-bank/bad-bank/src/components/Navbar.tsx
+++ bad-bank/bad-bank/src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react"
+import { useContext, useEffect, useState } from "react"
 import { Link, useLocation, useNavigate } from "react-router-dom"
 import { UserContext } from "../App"
 
@@ -21,28 +21,52 @@ export const Navbar = () => {
       'Transactions'
     ]
 
-  // function to return navbar links based on login status
-  const logFn: ReturnLoginStatusProps = (loginStatus) => {
-    const linkArray = loginStatus ? links.slice(2) : links.slice(0,2)
-    return (
-      linkArray.map(link => <Link className='link' key={link.replace(' ', '').toLowerCase()} to={`./${link.replace(' ', '').toLowerCase()}`}>{link}</Link>)
-    )
+    
+    // function to return navbar links based on login status
+    const logFn: ReturnLoginStatusProps = (loginStatus) => {
+      const linkArray = loginStatus ? links.slice(2) : links.slice(0,2)
+      return (
+        linkArray.map(link => <Link className='link' key={link.replace(' ', '').toLowerCase()} to={`./${link.replace(' ', '').toLowerCase()}`}>{link}</Link>)
+        )
   }
-
+  
   const navigate = useNavigate()
-
+  
+  // keep current page link highlighted
   const location = useLocation()
-
   useEffect(() => {
-    console.log(location.pathname)
     Array.from(document.getElementsByClassName('link')).forEach(( link: Element) => {
       if (link.getAttribute('href') === location.pathname) link.classList.add('active') 
       else link.classList.remove('active')
     })
   }, [ location.pathname ])
 
+  // display message under navbar when hovering links
+  const [infoMessage, setInfoMessage] = useState('')
+  useEffect(() => {
+    const info = [
+      ['Home', 'Return to the bank home page.'],
+      ['All Data', 'View Data for all bank customers.'],
+      ['Create Account', 'Create a new account (get a $50 bonus!).'],
+      ['Log Out', 'Log out of your account.'],
+      ['Login', 'Login to your account.'],
+      ['Deposit', 'Deposit funds into your account.'],
+      ['Withdraw', 'Withdraw funds from your account.'],
+      ['Transactions', 'View all transactions for your account.'],
+    ]
+
+    Array.from(document.getElementsByClassName('link')).forEach((link: Element) => {
+      link.addEventListener('mouseover', (event) => {
+        setInfoMessage(info.filter(i => (event.target as HTMLAnchorElement).innerText === i[0])[0][1] || 'message')
+      })
+      link.addEventListener('mouseout', () => {
+        setInfoMessage('')
+      })
+    })
+  }, [ctx.state.activeUser])
+
   return (
-    <nav className='navbar bg-primary d-flex m-0'>
+    <><nav className='navbar bg-primary d-flex m-0'>
       <div className='logo'>
         <Link to="./" className='text-light logo-text text-decoration-none'>BaddestBank</Link>
       </div>
@@ -57,5 +81,7 @@ export const Navbar = () => {
       }
       </>
     </nav>
+    <div className="bg-warning text-dark text-bold text-center">{ infoMessage }</div>
+    </>
   )
 }
